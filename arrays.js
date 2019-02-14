6325dba5ff781e28b0a28359010fcdf32e18908d
Pat Rabago <patriciorabago@gmail.com>
2019-02-14 22:32:00 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -26,7 +26,7 @@ function removeElementFromBeginningOfArray (array) {
 
 }
 function destructivelyRemoveElementFromEndOfArray (array) {
-  return array.shift()
+  return array.pop()
 }
 function removeElementFromEndOfArray (array) {
   return array.slice(0, array.length - 1)
